e8a4c5293753f6818c75b63a9854ffa143849eb6
Seillier Xavier <xseillier@gmail.com>
2024-07-21 16:06:21 +0200

    step 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html className={`${inter.className} antialiased`} lang="en">
       <body>{children}</body>
     </html>
   );
